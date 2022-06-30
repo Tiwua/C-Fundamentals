7e9ab2877a68862a4c477fcb6d6d2e8abb1c396a
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-06-30 22:56:50 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/01.Integer Operations/05.Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int lastNumber = int.Parse(Console.ReadLine());
+
+            for (int i = firstNumber; i <= lastNumber; i++)
+            {
+                char digit = (char)i;
+                Console.Write($"{digit} ");
+            }
+
+
+
+
+
+        }
+    }
+}
