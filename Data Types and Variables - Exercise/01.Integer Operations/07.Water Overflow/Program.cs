d10801eb8058438173fe573d4a0def5ba9ba27ee
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-07-03 23:15:11 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/01.Integer Operations/07.Water Overflow/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int waterTank = 255;
+
+
+            int firstLine = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= firstLine; i++)
+            {
+                int lines = int.Parse(Console.ReadLine());
+
+                if (lines <= waterTank)
+                {
+                    waterTank -= lines;
+                }
+                else
+
+                {
+                    Console.WriteLine("Insufficient capacity!");
+
+                }
+            }
+            int litesLeft = 255 - waterTank;
+
+            Console.WriteLine($"{litesLeft}");
+
+
+
+
+
+        }
+    }
+}
