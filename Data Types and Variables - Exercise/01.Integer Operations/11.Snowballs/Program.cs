8ccc0c98db1145b3030057d18c6405ddb6553810
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-07-08 00:19:56 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/01.Integer Operations/11.Snowballs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+namespace SnowBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowballCount = int.Parse(Console.ReadLine());
+
+            int highestSnoballSnow = 0;
+            int highestSnowBallTime = 0;
+            int highestSnowBallQuality = 0;
+            BigInteger highestSnowBallValue = BigInteger.Zero; ;
+
+
+            for (int i = 0; i < snowballCount; i++)
+            {
+
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                int snowballDivide = snowballSnow / snowballTime;
+                BigInteger snowBallValue = BigInteger.Pow(snowballDivide, snowballQuality);
+
+                if (snowBallValue >= highestSnowBallValue)
+                {
+                    highestSnoballSnow = snowballSnow;
+                    highestSnowBallTime = snowballTime;
+                    highestSnowBallQuality = snowballQuality;
+                    highestSnowBallValue = snowBallValue;
+                }
+
+            }
+            Console.WriteLine($"{highestSnoballSnow} : {highestSnowBallTime} = {highestSnowBallValue} ({highestSnowBallQuality})");
+
+        }
+    }
+}
