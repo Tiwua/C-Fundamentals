ef0b2fdfe4ad850950b967b1df8155af3e480921
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-07-13 15:12:42 +0300

    Add files via upload

---
+++ Arrays - Exercise/01.Train/02.CommonElements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            string[] secondInput = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < secondInput.Length; j++)
+                {
+                    if (input[i] == secondInput[j])
+                    {
+                        Console.Write($"{input[i]} ");
+                    }
+                }
+            }
+
+        }
+    }
+}
