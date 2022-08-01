4411adc23227f8c28ecba6843d6c09a33bc5e579
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-08-01 23:57:05 +0300

    Add files via upload

---
+++ Methods - Exercise/01.SmallestOfThreeNumbers/02.VowelsCount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string word = Console.ReadLine();
+
+            int vowelsCount = CountVowels(word);
+
+            Console.WriteLine(vowelsCount);
+
+        }
+        static int CountVowels(string word)
+        {
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            int vowelsCount = 0;
+            foreach (char ch in word.ToLower())
+            {
+                if (vowels.Contains(ch))
+                {
+                    vowelsCount++;
+                }
+            }
+            return vowelsCount;
+
+        }
+    }
+}
