77a243d38264c562b4633c8150adbf36f30be6bc
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-08-12 20:06:56 +0300

    Add files via upload

---
+++ 06. Middle Characters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            if (word.Length % 2 == 0)
+            {
+                GetMiddleOfWordEven(word);
+
+            }
+            else if (word.Length % 2 != 0)
+            {
+                GetMiddleOfWordOdd(word);
+            }
+
+        }
+
+        static int GetMiddleOfWordOdd(string word)
+        {
+            int length = word.Length;
+            int middle = length / 2;
+
+            Console.WriteLine(word[middle]);
+            return middle;
+        }
+
+        static int GetMiddleOfWordEven(string word)
+        {
+            int length = word.Length;
+            int middle = length / 2;
+
+            Console.WriteLine($"{word[middle - 1]}{word[middle]}");
+            return middle;
+        }
+
+
+
+    }
+}
