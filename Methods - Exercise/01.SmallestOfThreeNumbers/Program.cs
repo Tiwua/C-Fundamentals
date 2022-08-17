95b112ba66717f55d3adf52484733e212c0e5aaa
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-08-17 21:47:22 +0300

    Add files via upload

--- Methods - Exercise/01.SmallestOfThreeNumbers/Program.cs
+++ Methods - Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -1,80 +1,29 @@
 ﻿using System;
 using System.Linq;
 
-namespace ConsoleApp2
+namespace Solution
 {
-    internal class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            const int minLength = 6;
-            const int maxLength = 10;
-            const int minDigits = 2;
+            int number = int.Parse(Console.ReadLine());
+            int field = PrintSquareNxN(number);
 
-            string password = Console.ReadLine();
 
-            bool isPasswordValid = ValidatePassword(password, minLength, maxLength, minDigits);
-
-            if (isPasswordValid)
-            {
-                Console.WriteLine("Password is valid");
-            }
         }
 
-        static bool ValidatePassword(string password, int minLength, int maxLength, int minDigitCount)
+        static int PrintSquareNxN(int number)
         {
-            bool isPasswordValid = true;
-            if (!ValidatePasswordByCharacter(password, minLength, maxLength))
+            for (int row = 0; row < number; row++)
             {
-                Console.WriteLine($"Password must be between {minLength} and {maxLength} characters");
-                isPasswordValid = false;
-            }
-            if (!ValidatePasswordOfLettersAndDigits(password))
-            {
-                Console.WriteLine($"Password must consist only of letters and digits");
-                isPasswordValid = false;
-            }
-            if (!ValidatePasswordOfDigits(password, minDigitCount))
-            {
-                Console.WriteLine($"Password must have at least {minDigitCount} digits");
-                isPasswordValid = false;
-            }
-            return isPasswordValid;
-        }
-
-        static bool ValidatePasswordByCharacter(string password, int maxLength, int minLength)
-        {
-            if (password.Length <= minLength && password.Length >= maxLength)
-            {
-                return true;
-            }
-            return false;
-        }
-        static bool ValidatePasswordOfLettersAndDigits(string password)
-        {
-            foreach (char ch in password)
-            {
-                if (!Char.IsLetterOrDigit(ch))
+                for (int line = 0; line < number; line++)
                 {
-                    return false;
+                    Console.Write(number + " ");
                 }
+                Console.WriteLine();
             }
-            return true;
+            return number;
         }
-        static bool ValidatePasswordOfDigits(string password, int minDigitCount)
-        {
-            int digitsCount = 0;
-            foreach (char ch in password)
-            {
-                if (Char.IsDigit(ch))
-                {
-                    digitsCount++;
-
-                }
-            }
-            return digitsCount >= minDigitCount;
-        }
-
-
     }
 }
