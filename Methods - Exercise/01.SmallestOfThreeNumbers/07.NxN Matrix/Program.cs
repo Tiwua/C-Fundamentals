332d110a3b4cc3431fe3afd6485e257f1fe2b39a
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-08-17 21:47:41 +0300

    Add files via upload

---
+++ Methods - Exercise/01.SmallestOfThreeNumbers/07.NxN Matrix/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int field = PrintSquareNxN(number);
+
+
+        }
+
+        static int PrintSquareNxN(int number)
+        {
+            for (int row = 0; row < number; row++)
+            {
+                for (int line = 0; line < number; line++)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+            return number;
+        }
+    }
+}
