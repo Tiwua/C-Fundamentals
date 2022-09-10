bea81a352092181b3a9389bb4c6642d687015031
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-09-10 22:35:48 +0300

    Add files via upload

---
+++ Methods - Exercise/01.SmallestOfThreeNumbers/10TopNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+
+                if (FindSumOfDigits(i) && IncludeOddDigit(i))
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+        }
+        static bool FindSumOfDigits(int number)
+        {
+
+            int sumOfDigits = 0;
+
+            while (number != 0)
+            {
+                sumOfDigits += number % 10;
+                number /= 10;
+            }
+            bool isDivisable = sumOfDigits % 8 == 0;
+
+            return isDivisable;
+        }
+
+        static bool IncludeOddDigit(int number)
+        {
+            while (number > 0)
+            {
+                int currentDigit = number % 10;
+                if (currentDigit % 2 == 1)
+                {
+                    return true;
+                }
+                number /= 10;
+            }
+            return false;
+
+        }
+    }
+}
