c985e8ee2afd186bdc30539e08d8fc2a68e335f2
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-09-12 22:56:28 +0300

    Add files via upload

---
+++ Methods - Exercise/01.SmallestOfThreeNumbers/08.Factorial Division/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = int.Parse(Console.ReadLine());
+            double number2 = int.Parse(Console.ReadLine());
+
+            double division = DivideFactorialSum(number1, number2);
+
+
+
+        }
+        static double DivideFactorialSum(double number1, double number2)
+        {
+            double factioralSumOfNumber1 = 1;
+
+            for (double i = number1; i > 1; i--)
+            {
+                factioralSumOfNumber1 *= i;
+
+            }
+
+            double factioralSumOfNumber2 = 1;
+
+            for (double i = number2; i > 1; i--)
+            {
+                factioralSumOfNumber2 *= i;
+
+            }
+
+            double division = factioralSumOfNumber1 / factioralSumOfNumber2;
+            Console.WriteLine($"{division:f2}");
+
+            return division;
+        }
+    }
+}
