4b634013d002c00b9489a487eee33efaff95d980
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-09-13 22:59:00 +0300

    Add files via upload

---
+++ Methods - Exercise/01.SmallestOfThreeNumbers/09.Palindrome Integers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            IsTheNumberPalindrome(command);
+
+        }
+
+        static void IsTheNumberPalindrome(string command)
+        {
+            string reversed = string.Empty;
+
+            while (command != "END")
+            {
+                command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                for (int i = command.Length - 1; i >= 0; i--)
+                {
+                    reversed += command[i];
+                }
+
+                if (reversed != command)
+                {
+                    Console.WriteLine("false");
+                }
+                else
+                {
+                    Console.WriteLine("true");
+                }
+                reversed = String.Empty;
+            }
+        }
+    }
+}
