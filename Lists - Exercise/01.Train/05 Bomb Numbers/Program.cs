12a6bcc178447ce3958dc65913ca89620c81411a
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-09-24 20:52:31 +0300

    Add files via upload

---
+++ Lists - Exercise/01.Train/05 Bomb Numbers/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] bombInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int bombIndex = bombInfo[0];
+            int bombPower = bombInfo[1];
+
+            while (true)
+            {
+                int indexOfBomb = numbers.IndexOf(bombIndex);
+                if (indexOfBomb == -1)
+                {
+                    break;
+                }
+                ExplosionOfBomb(numbers, indexOfBomb, bombPower);
+
+            }
+            Console.WriteLine(numbers.Sum());
+
+        }
+
+        static void ExplosionOfBomb(List<int> numbers, int indexOfBomb, int bombPower)
+        {
+            int rightCount = indexOfBomb + bombPower;
+            for (int cnt = indexOfBomb; cnt <= rightCount; cnt++)
+            {
+                if (indexOfBomb >= numbers.Count)
+                {
+                    break;
+                }
+                numbers.RemoveAt(indexOfBomb);
+            }
+
+            int leftCount = indexOfBomb - bombPower;
+            if (leftCount < 0)
+
+            {
+                leftCount = 0;
+            }
+            for (int count = leftCount; count < indexOfBomb; count++)
+            {
+                numbers.RemoveAt(leftCount);
+            }
+        }
+    }
+}
