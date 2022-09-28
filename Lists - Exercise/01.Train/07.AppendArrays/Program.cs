76dcbc6693bb601112583daf8d21f0edb0748c86
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2022-09-28 23:14:39 +0300

    Add files via upload

---
+++ Lists - Exercise/01.Train/07.AppendArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split("|")
+                .Reverse()
+                .ToList();
+
+            List<string> resultList = new List<string>();
+
+            foreach (var element in list)
+            {
+                resultList.AddRange(element.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList());
+            }
+
+            Console.WriteLine(String.Join(" ", resultList));
+        }
+    }
+}
