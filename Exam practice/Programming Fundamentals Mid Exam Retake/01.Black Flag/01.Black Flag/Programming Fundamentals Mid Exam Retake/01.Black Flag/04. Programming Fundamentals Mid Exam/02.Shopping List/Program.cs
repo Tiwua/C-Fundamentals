ba2830fa457df294c13a25d0394ae30d66f0daa1
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2023-01-08 15:14:34 +0200

    Add files via upload

---
+++ Exam practice/Programming Fundamentals Mid Exam Retake/01.Black Flag/01.Black Flag/Programming Fundamentals Mid Exam Retake/01.Black Flag/04. Programming Fundamentals Mid Exam/02.Shopping List/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> groceries = Console.ReadLine()
+                .Split("!", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Go Shopping!")
+            {
+                string[] splittedCommand = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string action = splittedCommand[0];
+                if (action == "Urgent")
+                {
+                    string item = splittedCommand[1];
+                    if (groceries.Contains(item))
+                    {
+                        continue;
+                    }
+
+                    groceries.Insert(0, item);
+
+                }
+                else if (action == "Unnecessary")
+                {
+                    string item = splittedCommand[1];
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                    }
+                    continue;
+                }
+                else if (action == "Correct")
+                {
+                    string oldItem = splittedCommand[1];
+                    string newItem = splittedCommand[2];
+
+                    if (groceries.Contains(oldItem))
+                    {
+
+                        groceries.FindIndex(x => x == oldItem);
+
+                        int oldItemIndex = groceries.IndexOf(oldItem);
+                        groceries.Remove(oldItem);
+                        groceries.Insert(oldItemIndex, newItem);
+                    }
+                    continue;
+                }
+                else if (action == "Rearrange")
+                {
+                    string item = splittedCommand[1];
+                    if (groceries.Contains(item))
+                    {
+                        string renewedItem = item;
+                        groceries.Remove(item);
+                        groceries.Add(item);
+
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(", ", groceries));
+        }
+    }
+}
