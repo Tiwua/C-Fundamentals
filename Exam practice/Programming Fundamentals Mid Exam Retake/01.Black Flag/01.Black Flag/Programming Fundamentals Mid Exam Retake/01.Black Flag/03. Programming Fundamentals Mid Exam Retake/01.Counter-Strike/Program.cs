0d8a52216531cfcae0c0cf3f2b8e818a6426f68c
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2023-01-11 21:06:12 +0200

    Add files via upload

---
+++ Exam practice/Programming Fundamentals Mid Exam Retake/01.Black Flag/01.Black Flag/Programming Fundamentals Mid Exam Retake/01.Black Flag/03. Programming Fundamentals Mid Exam Retake/01.Counter-Strike/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            string command = string.Empty;
+            int count = 0;
+
+
+
+            while ((command = Console.ReadLine()) != "End of battle")
+            {
+                int distance = int.Parse(command);
+
+                if (energy < distance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {count} won battles and {energy} energy");
+                    return;
+                }
+                count++;
+                energy -= distance;
+                if (count % 3 == 0)
+                {
+                    energy += count;
+                }
+            }
+            Console.WriteLine($"Won battles: {count}. Energy left: {energy}");
+
+        }
+    }
+}
