20cfc7e36879b860fa791c08717f8f6c4b59cdb3
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2023-01-12 21:56:57 +0200

    Add files via upload

---
+++ Exam practice/Programming Fundamentals Mid Exam Retake/01.Black Flag/01.Black Flag/Programming Fundamentals Mid Exam Retake/01.Black Flag/03. Programming Fundamentals Mid Exam Retake/01.Counter-Strike/02.Shoot for the Win/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command;
+            int count = 0;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                int indexToShoot = int.Parse(command);
+
+                if (indexToShoot >= 0
+                    && indexToShoot < targets.Length
+                    && targets[indexToShoot] != -1)
+                {
+                    int power = targets[indexToShoot];
+                    targets[indexToShoot] = -1;
+                    for (int i = 0; i < targets.Length; i++)
+                    {
+
+                        if (targets[i] == -1)
+                        {
+                            continue;
+                        }
+                        if (targets[i] > power)
+                        {
+                            targets[i] -= power;
+                        }
+                        else
+                        {
+                            targets[i] += power;
+                        }
+
+                    }
+                    count++;
+                }
+            }
+            Console.WriteLine($"Shot targets: {count} -> {string.Join(" ", targets)}");
+        }
+    }
+}
