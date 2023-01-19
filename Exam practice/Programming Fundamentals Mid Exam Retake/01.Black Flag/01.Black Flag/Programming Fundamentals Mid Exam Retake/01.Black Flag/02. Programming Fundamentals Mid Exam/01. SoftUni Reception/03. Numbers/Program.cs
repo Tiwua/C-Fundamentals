284632350dfaa8827049582a952efba4764951c2
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2023-01-19 20:50:13 +0200

    Add files via upload

---
+++ Exam practice/Programming Fundamentals Mid Exam Retake/01.Black Flag/01.Black Flag/Programming Fundamentals Mid Exam Retake/01.Black Flag/02. Programming Fundamentals Mid Exam/01. SoftUni Reception/03. Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            double averageNum = numbers.Sum() / (double)numbers.Count;
+            double matchingNUmbersCOunt = numbers.Where(x => x > averageNum).ToArray().Count();
+
+            if (matchingNUmbersCOunt == 0)
+            {
+                Console.WriteLine("No");
+            }
+
+            else if (numbers.Count < 5)
+            {
+                Console.WriteLine("Less than 5 numbers");
+            }
+
+            else
+            {
+                var result = numbers.OrderByDescending(x => x).Where(x => x > averageNum).Take(5).ToArray();
+
+                Console.WriteLine(String.Join(" ", result));
+            }
+        }
+    }
+}
