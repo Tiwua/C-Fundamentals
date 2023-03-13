a253a10dec471808d454b34c725ddc41f823776e
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2023-03-13 23:10:15 +0200

    Add files via upload

---
+++ Exam practice/Programming Fundamentals Mid Exam Retake/01.Black Flag/01.Black Flag/Programming Fundamentals Mid Exam Retake/01.Black Flag/01. Programming Fundamentals Final Exam Retake/01.The Imitation Game/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string decryptedMessage = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Decode")
+            {
+                string[] commandArgs = command.Split("|", StringSplitOptions.RemoveEmptyEntries);
+                string action = commandArgs[0];
+
+                if (action == "Move")
+                {
+                    int lettersToMove = int.Parse(commandArgs[1]);
+
+                    string movedLetters = decryptedMessage.Substring(0, lettersToMove);
+                    decryptedMessage = decryptedMessage.Remove(0, lettersToMove);
+                    decryptedMessage = decryptedMessage + movedLetters;
+
+                }
+                else if (action == "Insert")
+                {
+                    int indexToInsert = int.Parse(commandArgs[1]);
+                    string value = commandArgs[2];
+
+                    decryptedMessage = decryptedMessage.Insert(indexToInsert, value);
+                }
+                else if (action == "ChangeAll")
+                {
+                    string substring = commandArgs[1];
+                    string replacement = commandArgs[2];
+
+                    if (decryptedMessage.Contains(substring))
+                    {
+                        decryptedMessage = decryptedMessage.Replace(substring, replacement);
+                    }
+                }
+            }
+            Console.WriteLine($"The decrypted message is: {decryptedMessage}");
+        }
+    }
+}
---
+++ Exam practice/Programming Fundamentals Mid Exam Retake/01.Black Flag/01.Black Flag/Programming Fundamentals Mid Exam Retake/01.Black Flag/01. Programming Fundamentals Final Exam Retake/03.The Pianist/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    class PianoPieces
+    {
+        public PianoPieces(string piece, string composer, string key)
+        {
+            this.Piece = piece;
+            this.Composer = composer;
+            this.Key = key;
+        }
+        public string Piece { get; set; }
+
+        public string Composer { get; set; }
+
+        public string Key { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPieces = int.Parse(Console.ReadLine());
+
+            List<PianoPieces> pianoCollection = new List<PianoPieces>();
+
+
+            for (int i = 0; i < numberOfPieces; i++)
+            {
+                string pieces = Console.ReadLine();
+                string[] input = pieces.Split('|');
+
+                string piece = input[0];
+                string composer = input[1];
+                string key = input[2];
+
+                pianoCollection.Add(new PianoPieces(piece, composer, key));
+            }
+            string command;
+            while ((command = Console.ReadLine()) != "Stop")
+            {
+
+                string[] splittedCommand = command
+                    .Split('|', StringSplitOptions.RemoveEmptyEntries);
+                string action = splittedCommand[0];
+                string piece = splittedCommand[1];
+
+                if (action == "Add")
+                {
+                    string composer = splittedCommand[2];
+                    string key = splittedCommand[3];
+                    PianoPieces currentPianoPiece = new PianoPieces(piece, composer, key);
+
+                    if (pianoCollection.FirstOrDefault(x => x.Piece == piece) == null)
+                    {
+                        pianoCollection.Add(currentPianoPiece);
+                        Console.WriteLine($"{piece} by {composer} in {key} added to the collection!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{piece} is already in the collection!");
+                    }
+                }
+                else if (action == "Remove")
+                {
+                    var itemToRemove = pianoCollection.FirstOrDefault(x => x.Piece == piece);
+                    if (itemToRemove != null)
+                    {
+                        pianoCollection.Remove(itemToRemove);
+                        Console.WriteLine($"Successfully removed {piece}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+
+                }
+                else if (action == "ChangeKey")
+                {
+                    string newKey = splittedCommand[2];
+
+                    var itemToCHangeKey = pianoCollection.FirstOrDefault(x => x.Piece == piece);
+
+                    if (itemToCHangeKey != null)
+                    {
+                        itemToCHangeKey.Key = newKey;
+                        Console.WriteLine($"Changed the key of {piece} to {newKey}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+            }
+
+            foreach (var song in pianoCollection)
+            {
+                Console.WriteLine($"{song.Piece} -> Composer: {song.Composer}, Key: {song.Key}");
+            }
+        }
+    }
+}
