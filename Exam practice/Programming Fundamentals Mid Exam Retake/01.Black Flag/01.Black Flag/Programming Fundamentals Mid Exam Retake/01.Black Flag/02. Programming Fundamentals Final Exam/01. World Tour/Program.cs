74041d2b85287a9fc57b385a7f1a51a0984b3eab
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2023-03-15 23:52:21 +0200

    Add files via upload

---
+++ Exam practice/Programming Fundamentals Mid Exam Retake/01.Black Flag/01.Black Flag/Programming Fundamentals Mid Exam Retake/01.Black Flag/02. Programming Fundamentals Final Exam/01. World Tour/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+                string[] commandArg = command.Split(":", StringSplitOptions.RemoveEmptyEntries);
+                string action = commandArg[0];
+
+                if (action == "Add Stop")
+                {
+                    int indexStop = int.Parse(commandArg[1]);
+                    string place = commandArg[2];
+
+                    if (indexStop >= 0 && indexStop < stops.Length)
+                    {
+                        stops = stops.Insert(indexStop, place);
+                    }
+
+                    Console.WriteLine(stops);
+                }
+
+                else if (action == "Remove Stop")
+                {
+                    int startIndex = int.Parse(commandArg[1]);
+                    int endIndex = int.Parse((commandArg[2]));
+
+                    if (startIndex >= 0 && endIndex < stops.Length)
+                    {
+                        stops = stops.Remove(startIndex, endIndex - startIndex + 1);
+                    }
+
+                    Console.WriteLine(stops);
+
+                }
+                else if (action == "Switch")
+                {
+                    string oldString = commandArg[1];
+                    string newString = commandArg[2];
+
+                    if (stops.Contains(oldString))
+                    {
+                        stops = stops.Replace(oldString, newString);
+                    }
+
+                    Console.WriteLine(stops);
+                }
+
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+
+        }
+    }
+}
