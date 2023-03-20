0791fc1d0b4e68d4c891eeb1fd350282c23f9eac
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2023-03-20 21:48:48 +0200

    Add files via upload

---
+++ Exam practice/Programming Fundamentals Mid Exam Retake/01.Black Flag/01.Black Flag/Programming Fundamentals Mid Exam Retake/01.Black Flag/03. Programming Fundamentals Final Exam Retake/01. Secret Chat/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp4
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Reveal")
+            {
+                string[] splittedCommand = command
+                    .Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = splittedCommand[0];
+
+                if (action == "InsertSpace")
+                {
+                    int indexToInsert = int.Parse(splittedCommand[1]);
+
+                    input = input.Insert(indexToInsert, " ");
+
+                }
+                else if (action == "Reverse")
+                {
+                    string substring = splittedCommand[1];
+                    if (input.Contains(substring))
+                    {
+                        int indexToCut = input.IndexOf(substring);
+                        input = input.Remove(indexToCut, substring.Length);
+                        input = input + string.Join("", substring.Reverse());
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    }
+                }
+                else if (action == "ChangeAll")
+                {
+                    string substring = splittedCommand[1];
+                    string replacement = splittedCommand[2];
+
+                    input = input.Replace(substring, replacement);
+                }
+                Console.WriteLine(input);
+            }
+            Console.WriteLine($"You have a new text message: {input}");
+        }
+    }
+}
